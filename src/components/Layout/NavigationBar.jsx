409e233b3ed29c6db76a8e4fdd2ec3772989d641
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-06-22 12:59:17 +0200

    Layout now is sticky

--- src/components/Layout/NavigationBar.jsx
+++ src/components/Layout/NavigationBar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 const NavigationBar = () => {
   return (
     <>
-      <nav className="bg-[#201f1f] [&>*]:px-4 w-[100%] p-0 [&>*]:my-auto h-[2.8rem] flex flex-row  text-white">
+      <nav className="bg-[#201f1f] sticky z-10 top-0 [&>*]:px-4 w-[100%] p-0 [&>*]:my-auto h-[2.8rem] flex flex-row  text-white">
         <Link to="/" className="self-start">KINOMORE</Link>
         <div className="justify-center [&>.link]:xl:hidden [&>a]:px-5 flex w-[100%]">
         <Link to="/movies" className="link">Movies</Link>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,17 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 const SideBarMenu = () => {
   return (
-    <div className="flex max-xl:hidden [&>*]:px-4 [&>*]:py-2 w-[250px] flex-col bg-[#333] text-white h-[100%]">
-      <Link to="/">Home</Link>
-      <Link to="/movies">Movies</Link>
-      <Link to="/tv-shows">TV Shows</Link>
+    <div className="min-h-[100%]">
+      <div className="sticky top-[2.8rem]">
+        <div className="flex box-border max-xl:hidden [&>*]:px-4 [&>*]:py-2 w-[250px] flex-col bg-[#333] text-white min-h-[100%]">
+          <Link to="/">Home</Link>
+          <Link to="/movies">Movies</Link>
+          <Link to="/tv-shows">TV Shows</Link>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default SideBarMenu;
