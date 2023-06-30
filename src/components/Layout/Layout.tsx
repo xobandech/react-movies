38d9db3f1cad1076ddcc52ce99504eab4736dc1b
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-06-30 17:41:37 +0200

    Fix error on netlify

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 import NavigationBar from "./NavigationBar.tsx";
 import Footer from "./Footer.tsx";
 import PageContainer from "../PageContainer";
-import SideBarMenu from "./Sidebar";
+import SideBarMenu from "./Sidebar.tsx";
 const Layout = () => {
   return (
     <>
