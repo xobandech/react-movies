9eb00c51a63096cc6a05fcfba5f9a3e9b36fb9db
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-06-30 15:47:16 +0200

    Add pagination & memoization to prev&next movies page

--- src/pages/GenrePage.jsx
+++ src/pages/GenrePage.jsx
@@ -1,23 +1,29 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import MovieCard from "../components/Cards/MovieCard";
 import { GENRES } from "../constants/genres.ts";
+
 const apiKey = process.env.REACT_APP_KINOPOISK_API_KEY;
 
 const GenrePage = () => {
   const { genre } = useParams();
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [currentPage, setCurrentPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState(0);
   const [loadedPages, setLoadedPages] = useState([]);
 
   useEffect(() => {
     const fetchMoviesByGenre = async () => {
       setIsLoading(true);
-      
+
       try {
+        // Check if movies for the current page are already loaded
+        if (loadedPages.includes(currentPage)) {
+          return;
+        }
+
         const response = await fetch(
-          `https://api.kinopoisk.dev/v1.3/movie?page=${currentPage}&limit=12&genres.name=${GENRES[genre]}`,
+          `https://api.kinopoisk.dev/v1.3/movie?page=${currentPage + 1}&limit=36&genres.name=${GENRES[genre]}`,
           {
             method: "GET",
             headers: {
@@ -29,13 +35,12 @@ const GenrePage = () => {
 
         const data = await response.json();
         console.log("fetch");
-        
-        if (!loadedPages.includes(currentPage)) {
-          const filteredMovies = data.docs.filter(
-            (movie) => movie.name !== "" && movie.poster
-          );
-          setMovies((prevMovies) => [...prevMovies, ...filteredMovies]);
-        }
+        setMovies((prevMovies) => {
+          const updatedMovies = [...prevMovies];
+          updatedMovies[currentPage] = data.docs;
+          return updatedMovies;
+        });
+        console.log(movies);
         setLoadedPages((prevLoadedPages) => [...prevLoadedPages, currentPage]);
       } catch (error) {
         console.error(error);
@@ -47,24 +52,6 @@ const GenrePage = () => {
     fetchMoviesByGenre();
   }, [currentPage, genre, loadedPages]);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const { scrollTop, scrollHeight, clientHeight } =
-        document.documentElement;
-      const isScrolledToEnd = scrollTop + clientHeight >= scrollHeight - 5;
-      if (isScrolledToEnd && !isLoading) {
-        setCurrentPage((prevPage) => prevPage + 1);
-        console.log(movies);
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [currentPage]);
-
   if (isLoading) {
     return <h1>Loading...</h1>;
   }
@@ -72,12 +59,33 @@ const GenrePage = () => {
   return (
     <div className="w-[1030px] bg-white">
       <h1>Genres</h1>
-    
+
       <div className="grid gap-5 justify-items-center grid-cols-3 max-md:grid-cols-2 px-10 ">
-        {movies.map((movie) => {
-          return <MovieCard movie={movie} key={movie.id} />;
-        })}
+        {movies[currentPage] &&
+          movies[currentPage].map((movie) => {
+            return <MovieCard movie={movie} key={movie.id} />;
+          })}
       </div>
+      <button
+        onClick={() => {
+          if (currentPage > 0) {
+            setCurrentPage(currentPage - 1);
+            window.scrollTo({ top: 0, behavior: "smooth" });
+          }
+        }}
+        disabled={currentPage < 1}
+      >
+        Previous Page
+      </button>
+
+      <button
+        onClick={() => {
+          setCurrentPage(currentPage + 1);
+          window.scrollTo({ top: 0, behavior: "smooth" });
+        }}
+      >
+        Next Page
+      </button>
     </div>
   );
 };
